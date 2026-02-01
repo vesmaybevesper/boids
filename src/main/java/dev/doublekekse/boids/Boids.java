8a6afb595e5794b30ca8863dd47f9aaff6bb53bb
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-02-01 16:08:02 -0500

    updated to 1.21.11

--- src/main/java/dev/doublekekse/boids/Boids.java
+++ src/main/java/dev/doublekekse/boids/Boids.java
@@ -7,7 +7,9 @@ import net.minecraft.core.Holder;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
+import net.minecraft.server.permissions.Permission;
+import net.minecraft.server.permissions.Permissions;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 
@@ -52,7 +54,7 @@ public class Boids implements ModInitializer {
 
     private static Collection<? extends EntityType<?>> getEntities(List<String> ids) {
         return ids.stream()
-            .map(ResourceLocation::tryParse)
+            .map(Identifier::tryParse)
             .filter(Objects::nonNull)
             .map(BuiltInRegistries.ENTITY_TYPE::get)
             .filter(Optional::isPresent)
@@ -72,7 +74,7 @@ public class Boids implements ModInitializer {
         CommandRegistrationCallback.EVENT.register(
             (dispatcher, registryAccess, environment) -> {
                 dispatcher.register(
-                    literal("boids").requires((source) -> source.hasPermission(2)).then(literal("config").then(literal("reload").executes(ctx -> {
+                    literal("boids").requires((source) -> source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR)).then(literal("config").then(literal("reload").executes(ctx -> {
                         loadConfig();
 
                         ctx.getSource().sendSuccess(() -> Component.translatable("commands.boids.config.reload"), true);
